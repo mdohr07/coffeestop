0ac0398f9b7021e7056dfdd1ae190aca2f3257d0
m-dohr-07 <mdohr07@gmail.com>
2023-08-29 13:03:23 +0200

    form field

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
+var menu = document.getElementById("menu");
 
+menu.onclick = function() {
+    menu.classList.toggle("openmenu");
+}
+
+// Script für Galerie-Scrolling
+let scrollContainer = document.querySelector(".specialities__gallery");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+evt.preventDefault();
+scrollContainer.scrollLeft += evt.deltaY;
+scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+scrollContainer.style.scrollBehavior = "smooth";
+scrollContainer.scrollLeft += 1000;
+});
+
+prevBtn.addEventListener("click", () => {
+scrollContainer.style.scrollBehavior = "smooth";
+scrollContainer.scrollLeft -= 1000;
+});
+
+// Dieses Skript wird ausgeführt, nachdem das DOM vollständig geladen ist
+document.addEventListener("DOMContentLoaded", function() {
+// Scrollbar ausblenden
+const gallery = document.querySelector(".specialities__gallery");
+gallery.style.overflow = "hidden";
+});
+
+
+//Loading one by one
 /* Takes callback function into its constructor, can obersve multipe elements 
 or entries at the same time and runs every time the visibility of one of the 
 elements changes */
